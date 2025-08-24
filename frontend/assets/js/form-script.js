51fe57afb586ffe583ba7c33bf8c51a13d33e0c7
Crimen. <sebastiianlopez021016@gmail.com>
2025-08-24 14:23:03 -0500

    Update 24/08/2025

--- frontend/assets/js/form-script.js
+++ frontend/assets/js/form-script.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 document.addEventListener('DOMContentLoaded', async function () {
     // Configuración de Supabase
